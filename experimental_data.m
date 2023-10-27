a18b15b36a63e6369bd3c3b5b86f9cb9a93d275e
CharlesChoi11 <149180633+CharlesChoi11@users.noreply.github.com>
2023-10-27 17:34:22 +0100

    start plot
    11.Turbine isentropic efficiency vs. turbine corrected mass flow rate

--- experimental_data.m
+++ experimental_data.m
@@ -13,33 +13,64 @@
 experimentalData = readtable("Test Data B1.xlsx");
 
 % Separating the columns
-throttlePosition = experimentalData(:,3);       % represented as a percentage
-inletT2 = experimentalData(:,4);                % absolute temperature at station T2's inlet
-inletT3 = experimentalData(:,5);                % absolute temperature at compressor entry(station T3)
-exitT4 = experimentalData(:,6);                 % absolute,total temperature at combustor exit(station T4)
-exitT5 = experimentalData(:,7);                 % absolute, total temperature at turbine exit(station T5)
-exitT6 = experimentalData(:,8);                 % absolute, total nozzle exhaust temperature(station T6)
-ambPressure = experimentalData(:,9);            % ambient pressure(P_0)
-
-inletP1 = experimentalData(:,12);               % static inlet presure(p_1)
-
-exitP3 = experimentalData(:,15);                % static compressor exit pressure(p_3) at station P3
-
-inletP3 = experimentalData(:,18);               % total combustor inlet pressure(P_3) at station P3
-
-inletP4 = experimentalData(:,21);               % absolute turbine inlet static pressure(P_4) at station P4
-
-entryP6 = experimentalData(:,24);               % absolute nozzle entry pressure(P_6) at station P6
-
-thrust = experimentalData(:,27);                % thrust in [N]
-speed = experimentalData(:,28);                 % speed in [rpm]
-fuelFlow = experimentalData(:,29);              % fuel flow in [l/min]
-density = experimentalData(:,30);               % upstream density in [kg/m^3]
-massFlow = experimentalData(:,31);              % mass flow rate in [kg/s]
-specHeat1 = experimentalData(:,32);             % specific heat(C_p_1) in [kJ/kg.K]
-specHeat2 = experimentalData(:,33);             % specific heat(C_p_2) in [kJ/kg.K]
-specHeat3 = experimentalData(:,34);             % specific heat(C_p_3) in [kJ/kg.K]
-specHeat4 = experimentalData(:,35);             % specific heat(C_p_4) in [kJ/kg.K]
-specHeat5 = experimentalData(:,36);             % specific heat(C_p_5) in [kJ/kg.K]
+throttlePosition = table2array(experimentalData(:,3)); % represented as a percentage
+inletT2 = table2array(experimentalData(:,4)); % absolute temperature at station T2's inlet
+inletT3 = table2array(experimentalData(:,5)); % absolute temperature at compressor entry(station T3)
+exitT4 = table2array(experimentalData(:,6)); % absolute,total temperature at combustor exit(station T4)
+exitT5 = table2array(experimentalData(:,7)); % absolute, total temperature at turbine exit(station T5)
+exitT6 = table2array(experimentalData(:,8)); % absolute, total nozzle exhaust temperature(station T6)
+ambPressure = table2array(experimentalData(:,9)); % ambient pressure(P_0)
+ 
+inletP1 = table2array(experimentalData(:,12)); % static inlet presure(p_1) in [kPa]
+ 
+exitP3 = table2array(experimentalData(:,15)); % static compressor exit pressure(p_3) at station P3 in [kPa]
+ 
+inletP3 = table2array(experimentalData(:,18)); % total combustor inlet pressure(P_3) at station P3 in [kPa]
+ 
+inletP4 = table2array(experimentalData(:,21)); % absolute turbine inlet static pressure(P_4) at station P4 in [kPa]
+ 
+entryP6 = table2array(experimentalData(:,24)); % absolute nozzle entry pressure(P_6) at station P6 in [kPa]
+ 
+thrust = table2array(experimentalData(:,27)); % thrust in [N]
+spoolSpeed = table2array(experimentalData(:,28)); % spool speed in [rpm]
+fuelFlow = table2array(experimentalData(:,29)); % fuel flow in [l/min]
+density = table2array(experimentalData(:,30)); % upstream density in [kg/m^3]
+massFlow = table2array(experimentalData(:,31)); % mass flow rate in [kg/s]
+specHeat1 = table2array(experimentalData(:,32)); % specific heat(C_p_1) in [kJ/kg.K]
+specHeat2 = table2array(experimentalData(:,33)); % specific heat(C_p_2) in [kJ/kg.K]
+specHeat3 = table2array(experimentalData(:,34)); % specific heat(C_p_3) in [kJ/kg.K]
+specHeat4 = table2array(experimentalData(:,35)); % specific heat(C_p_4) in [kJ/kg.K]
+specHeat5 = table2array(experimentalData(:,36)); % specific heat(C_p_5) in [kJ/kg.K]
+ 
+% Reference values
+tempRef = 288.15; % reference temperature at sea level on a standard day in [K]
+pressRef = 101.325; % reference pressure at sea level on a standard day in [Pa]
+spoolRef = 108000; % reference spool speed in [rpm]
 
 %% Plotting the data
+
+% 11.Turbine isentropic efficiency vs. turbine corrected mass flow rate
+
+
+exitT4 % Turbine inlet temperature (K)
+exitT5   % Turbine outlet temperature (K)
+tempout_isentropic   % Isentropic turbine outlet temperature (K)
+pressin % Turbine inlet pressure (Pa)
+pressout   % Turbine outlet pressure (Pa)
+tempRef   % Reference temperature (K)
+pressRef   % Reference pressure (Pa)
+mass_flow_rate_actual % Actual mass flow rate (kg/s)
+
+% Calculate turbine isentropic efficiency
+eta_t = (exitT4 - exitT5) ./ (exitT4 - tempout_isentropic);
+
+% Calculate turbine corrected mass flow rate
+mass_flow_rate_corrected = mass_flow_rate_actual .* (exitT4 ./ tempRef).^0.5 .* (inletP4 ./ pressRef);
+
+% Plot
+figure;
+plot(mass_flow_rate_corrected, eta_t, '-o');
+xlabel('Turbine Corrected Mass Flow Rate (kg/s)');
+ylabel('Turbine Isentropic Efficiency');
+title('Turbine Isentropic Efficiency vs. Corrected Mass Flow Rate');
+grid on;
