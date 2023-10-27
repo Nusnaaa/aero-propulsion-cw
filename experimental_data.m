2d11366cc3743b7c88e1aa10dcc7661751e98642
Nusnaaa <137788297+Nusnaaa@users.noreply.github.com>
2023-10-27 15:32:50 +0100

    Removed unnecessary data
    This data is not relevant to the analysis. It was hidden on excel and serves no use.

--- experimental_data.m
+++ experimental_data.m
@@ -9,23 +9,17 @@ exitT4 = experimentalData(:,6);                 % absolute,total temperature at 
 exitT5 = experimentalData(:,7);                 % absolute, total temperature at turbine exit(station T5)
 exitT6 = experimentalData(:,8);                 % absolute, total nozzle exhaust temperature(station T6)
 ambPressure = experimentalData(:,9);            % ambient pressure(P_0)
-inletRaw = experimentalData(:,10);
-inletOffset = experimentalData(:,11);
+
 inletP1 = experimentalData(:,12);               % static inlet presure(p_1)
-compRaw = experimentalData(:,13);
-compOffset = experimentalData(:,14);
+
 exitP3 = experimentalData(:,15);                % static compressor exit pressure(p_3) at station P3
-combRaw = experimentalData(:,16);
-combOffset = experimentalData(:,17);
+
 inletP3 = experimentalData(:,18);               % total combustor inlet pressure(P_3) at station P3
-turbRaw = experimentalData(:,19);
-turbOffest = experimentalData(:,20);
+
 inletP4 = experimentalData(:,21);               % absolute turbine inlet static pressure(P_4) at station P4
-exhaustRaw = experimentalData(:,22);
-exhaustOffset = experimentalData(:,23);
+
 entryP6 = experimentalData(:,24);               % absolute nozzle entry pressure(P_6) at station P6
-thrustRaw = experimentalData(:,25);
-thrustOffset = experimentalData(:,26);
+
 thrust = experimentalData(:,27);                % thrust in [N]
 speed = experimentalData(:,28);                 % speed in [rpm]
 fuelFlow = experimentalData(:,29);              % fuel flow in [l/min]
